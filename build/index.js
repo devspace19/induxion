e8b2b3656f3de1ed1bc2fc9c0fb7bd114e301c63
Muhammad Al Ziqri <muhammadalziqri@gmail.com>
2019-10-27 23:29:51 +0700

    build

--- build/index.js
+++ build/index.js
@@ -21,5 +21,3 @@ var _inject = _interopRequireDefault(require("./inject"));
 var _provider = _interopRequireDefault(require("./provider"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-console.log('test');
