9dea71e09f510366d4f51e644459da9c1cb252a1
Nurimansyah Rifwan <nurimansyah.rifwan@gmail.com>
2019-10-31 18:49:43 +0700

    feat: update layout

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -1,15 +1,17 @@
 const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass');
+const touch = require('gulp-touch-cmd');
 
 const config = {
-    src: '_styles/main.scss',
+    src: '_styles/**/*.scss',
     dest: 'assets/css'
 };
 
 const SCSSTask = () => {
     return src(config.src)
         .pipe(sass().on('error', sass.logError))
-        .pipe(dest(config.dest));
+        .pipe(dest(config.dest))
+        .pipe(touch());
 };
 
 const defaultTask = cb => {
---
+++ docs/usage/storage.md
@@ -0,0 +1,5 @@
+---
+layout: two-columns
+---
+
+# Storage
